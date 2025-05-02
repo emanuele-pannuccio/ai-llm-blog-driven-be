25e04fea0e38cbc56373356c7ba0b366f7bdde9a
Your Name <you@example.com>
2025-05-03 01:41:28 +0200

    fix: old port

--- app/main.py
+++ app/main.py
@@ -91,4 +91,4 @@ if __name__ == '__main__':
                 db.session.add(status_orm)
                 db.session.commit()
 
-    app.run(host='0.0.0.0', port="8081", debug=True)
+    app.run(host='0.0.0.0', port="8080", debug=True)
